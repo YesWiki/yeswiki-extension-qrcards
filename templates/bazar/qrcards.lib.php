4bd378da16a6e44311e52d4e5633ff1668e46487
mrflos <mrflos@lilo.org>
2023-02-04 23:05:03 +0300

    refact(qrcard) : twig for qrcards, css variables

--- templates/bazar/qrcards.lib.php
+++ templates/bazar/qrcards.lib.php
@@ -43,9 +43,9 @@ function display_difficulty($fiche)
     if ($fiche['listeListePriseEnMain'] == 'facile') {
         return '⚫️';
     } elseif ($fiche['listeListePriseEnMain'] == 'moyen') {
-        return '⚫️⚫️';
+        return '⚫️ ⚫️';
     } elseif ($fiche['listeListePriseEnMain'] == 'complexe') {
-        return '⚫️⚫️⚫️';
+        return '⚫️ ⚫️ ⚫️';
     } else {
         return '';
     }
@@ -59,28 +59,16 @@ function displayCard($fiche, $view='print')
     $thumbresize = 'fit';
     $imgLogo = 'tools/qrcards/images/metacartes.png';
     $defaultMiniIcon = 'tools/qrcards/images/metacartes.png';
-    $classprefix = ($view == 'print') ? 'print-' : '';
-    $output = '<div class="qrcard '.$classprefix.'front" style="background-color: '.$fiche['bf_card_color'].';">
-            <div class="qrcard-header">
-              <div class="complexity">'.display_difficulty($fiche).'</div>
-              <h1 class="qrcard-title">'.$fiche['bf_titre'].'</h1>
-            </div>
-            <div class="qrcard-main-content">';
-
     if (isset($fiche['imagebf_image']) and is_file('files/'.$fiche['imagebf_image'])) {
-        $output .= '<div class="qrcard-img">
-                <img alt="image" src="'.redimensionner_image(
-            'files/' . $fiche['imagebf_image'],
-            'cache/image_' . $thumbwidth . 'x' . $thumbheight . '_' . $fiche['imagebf_image'],
-            $thumbwidth,
-            $thumbheight,
-            $thumbresize
-        ).'" />
-              </div>';
+      $image = '<img alt="image" src="'.redimensionner_image(
+          'files/' . $fiche['imagebf_image'],
+          'cache/image_' . $thumbwidth . 'x' . $thumbheight . '_' . $fiche['imagebf_image'],
+          $thumbwidth,
+          $thumbheight,
+          $thumbresize
+      ).'" />';
     } else {
-        $output .= '<div class="qrcard-img">
-                <img alt="logo metacartes" src="'.$imgLogo.'" />
-              </div>';
+        $image = '<img alt="logo qrcard" src="'.$imgLogo.'" />';
     }
     $picto1 = display($fiche['bf_picto_boite1'], $defaultMiniIcon, true, $fiche['bf_texte_boite1']);
     if (!empty($picto1)) {
@@ -94,56 +82,30 @@ function displayCard($fiche, $view='print')
     if (!empty($picto3)) {
         $picto3 = '<img src="'.$picto3.'" alt="mini-picto3" />';
     }
-    $output .= '<div class="qrcard-text">
-                <span>'.$fiche['bf_accroche'].'</span>
-              </div>
-            </div>
-            <div class="qrcard-footer">
-              <img class="logo" alt="logo"
-                src="'.$imgLogo.'" />
-              <div class="boite1">
-                <div class="mini-picto">'.$picto1.'</div>
-                <div class="mini-text">'.display($fiche['bf_texte_boite1']).'</div>
-              </div>
-              <div class="boite2">
-                <div class="mini-picto">'.$picto2.'</div>
-                <div class="mini-text">'.display($fiche['bf_texte_boite2']).'</div>
-              </div>
-              <div class="boite3">
-                <div class="mini-picto">'.$picto3.'</div>
-                <div class="mini-text">'.display($fiche['bf_texte_boite3']).'</div>
-              </div>
-            </div>
-          </div>
-          <div class="qrcard '.$classprefix.'back" style="background-color: '.$fiche['bf_card_color'].';">
-            <div class="qrcard-header">
-              <div class="complexity">'.display_difficulty($fiche).'</div>
-              <h1 class="qrcard-title">'.$fiche['bf_titre'].'</h1>
-            </div>
-            <div class="qrcard-main-content">
-              <div class="qrcard-longtext">
-                <p class="verso1">'.f($fiche['bf_essentiel']).'</p>
-              </div>
-            </div>
-            <div class="qrcard-footer">
-              <img class="logo" alt="logo"
-                src="'.$imgLogo.'" />
-              <div class="link">
-                <a class="card-link"
-                  href="'.$GLOBALS['wiki']->href('', $fiche['id_fiche']).'">'.str_replace(array('https://', 'http://'), '', $GLOBALS['wiki']->href('', $fiche['id_fiche'])).'</a>
-                <br /><img class="logo-cc" src="https://mirrors.creativecommons.org/presskit/buttons/88x31/png/by-sa.png"
-                  alt="licence CC BY SA" />
-              </div>
-              <div class="qrcode">
-                '.$GLOBALS['wiki']->format('{{qrcode text="'.$GLOBALS['wiki']->href('', $fiche['id_fiche']).'"}}').'
-              </div>
-            </div>
-        </div>
-        <div class="clearfix"></div>';
-    if ($view != 'print') {
-        $output =  '<div class="bazar-entry flip-container" ontouchstart="this.classList.toggle(\'hover\');" '.$fiche['html_data'].'><div><div class="flipper">'.$output.'</div></div></div>';
-    } else {
-        $output = '<div class="single-qrcard-container">'.$output.'</div>';
+    $link = (!empty($fiche['bf_url'])) ? $fiche['bf_url'] : $GLOBALS['wiki']->href('', $fiche['id_fiche']);
+    $types = baz_valeurs_liste('ListeTypeCarte');
+    $type = '';
+    if (!empty($fiche['listeListeTypeCarte']) && !empty($types['label'][$fiche['listeListeTypeCarte']])) {
+        $type = $types['label'][$fiche['listeListeTypeCarte']];
     }
+    $elements = [
+      'fiche' => $fiche,
+      'view' => $view,
+      'difficulty' => display_difficulty($fiche),
+      'type' => $type,
+      'logoimage' => $imgLogo,
+      'mainimage' => $image,
+      'picto1' => $picto1,
+      'picto2' => $picto2,
+      'picto3' => $picto3,
+      'textpicto1' => display($fiche['bf_texte_boite1']),
+      'textpicto2' => display($fiche['bf_texte_boite2']),
+      'textpicto3' => display($fiche['bf_texte_boite3']),
+      'longtext' => f($fiche['bf_essentiel']),
+      'qrcode' => $GLOBALS['wiki']->format('{{qrcode text="'.$link.'"}}'),
+      'link' => $link,
+      'shortlink' => str_replace(array('https://', 'http://'), '', $link)
+    ];
+    $output = $GLOBALS['wiki']->render("@qrcards/card-layouts/qrcard.twig", $elements);
     return $output;
 }
