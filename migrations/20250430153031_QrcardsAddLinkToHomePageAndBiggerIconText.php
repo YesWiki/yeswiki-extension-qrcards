8f977576dfe1c93acf1fc445e23a300c246e2578
Florian Schmitt <mrflos@gmail.com>
2025-04-30 19:32:44 +0300

    feat: rename categories + adaptative titles

--- migrations/20250430153031_QrcardsAddLinkToHomePageAndBiggerIconText.php
+++ migrations/20250430153031_QrcardsAddLinkToHomePageAndBiggerIconText.php
@@ -1,6 +1,7 @@
 <?php
 
 use YesWiki\Core\Service\DbService;
+use YesWiki\Core\Service\PageManager;
 use YesWiki\Core\YesWikiMigration;
 
 class QrcardsAddLinkToHomePageAndBiggerIconText extends YesWikiMigration
@@ -8,6 +9,10 @@ class QrcardsAddLinkToHomePageAndBiggerIconText extends YesWikiMigration
     public function run(): void
     {
         $dbService = $this->getService(DbService::class);
+        $pageManager = $this->getService(PageManager::class);
+
+        // update to new ListeTypeCarte
+        $pageManager->save('ListeTypeCarte', file_get_contents('tools/qrcards/setup/lists/ListeTypeCarte.json'));
 
         // add link to wikicarte to existing wikis
         $dbService->query("UPDATE {$dbService->prefixTable('pages')} SET `body` = REPLACE( `body` , '{{button icon=\"fas fa-book-reader\" link=\"DocumentationQrCartes\" nobtn=\"1\" text=\"Documentation\" }}\r\n', '{{button icon=\"fas fa-book-reader\" link=\"DocumentationQrCartes\" nobtn=\"1\" text=\"Documentation\" }}\r\n - {{button icon=\"fas fa-code-branch\" link=\"https://metacartes.net/wikicartes/\" nobtn=\"1\" text=\"Portail wikicartes\" }}\r\n' ) WHERE tag='PageRapideHaut' AND latest='Y'");
