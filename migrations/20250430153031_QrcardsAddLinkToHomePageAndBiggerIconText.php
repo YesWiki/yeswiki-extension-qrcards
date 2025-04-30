e445a42697c51caf0184ea13729aee6d3ff20084
Florian Schmitt <mrflos@gmail.com>
2025-04-30 17:19:05 +0300

    new migration

---
+++ migrations/20250430153031_QrcardsAddLinkToHomePageAndBiggerIconText.php
@@ -0,0 +1,18 @@
+<?php
+
+use YesWiki\Core\Service\DbService;
+use YesWiki\Core\YesWikiMigration;
+
+class QrcardsAddLinkToHomePageAndBiggerIconText extends YesWikiMigration
+{
+    public function run(): void
+    {
+        $dbService = $this->getService(DbService::class);
+
+        // add link to wikicarte to existing wikis
+        $dbService->query("UPDATE {$dbService->prefixTable('pages')} SET `body` = REPLACE( `body` , '{{button icon=\"fas fa-book-reader\" link=\"DocumentationQrCartes\" nobtn=\"1\" text=\"Documentation\" }}\r\n', '{{button icon=\"fas fa-book-reader\" link=\"DocumentationQrCartes\" nobtn=\"1\" text=\"Documentation\" }}\r\n - {{button icon=\"fas fa-code-branch\" link=\"https://metacartes.net/wikicartes/\" nobtn=\"1\" text=\"Portail wikicartes\" }}\r\n' ) WHERE tag='PageRapideHaut' AND latest='Y'");
+
+        // change form for longer inputs
+        $dbService->query("UPDATE {$dbService->prefixTable('nature')} SET `bn_template` = REPLACE( `bn_template` , '***15***15', '***30***30' ) WHERE bn_id_nature=1400");
+    }
+}
